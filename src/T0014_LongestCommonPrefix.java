648fa39ff126d6da6c8ba96de6443c14984fe589
Canon <717437709@qq.com>
2021-01-13 21:41:53 +0800

    Longest Common Prefix 第二版 优化

--- src/T0014_LongestCommonPrefix.java
+++ src/T0014_LongestCommonPrefix.java
@@ -38,9 +38,16 @@ public class T0014_LongestCommonPrefix {
      * 如果都相等继续往下执行
      * 如果存在不相等或错误 则表示某一个成员的第index位不是共有的前缀
      * 取任意一个成员截取 0 ~ index-1 位 即为公共前缀
-     *
      * 执行用时：7 ms, 在所有 Java 提交中击败了12.01%的用户
      * 内存消耗：38.8 MB, 在所有 Java 提交中击败了5.06%的用户
+     *  ------------------------
+     *  第二版:
+     *  在 for 循环体内有一处: char[] chars = strs[i].toCharArray();
+     *  然后通过 chars[index] 作比较
+     *  将 toCharArray 这波操作改成 strs[i].charAt(index) 性能大大改善
+     *
+     *  执行用时：1 ms, 在所有 Java 提交中击败了85.79%的用户
+     *  内存消耗：37 MB, 在所有 Java 提交中击败了19.40%的用户
      *
      */
     public static String longestCommonPrefix2(String[] strs) {
@@ -51,12 +58,12 @@ public class T0014_LongestCommonPrefix {
             char temp = '\0';
             for (int i = 0; i < strs.length; i++) {
                 try {
-                    char[] chars = strs[i].toCharArray();
+                    char c = strs[i].charAt(index);
                     if (i == 0){
-                        temp = chars[index];
+                        temp = c;
                         continue;
                     }
-                    if (temp != chars[index]){
+                    if (temp != c){
                         return strs[0].substring(0,index);
                     }
                 }catch (Exception e){
