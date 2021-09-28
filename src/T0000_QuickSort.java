1930c84e10dabec77d93d343a2d5901ed9a41a25
canon <717437709@qq.com>
2021-09-28 18:43:20 +0800

    快排

---
+++ src/T0000_QuickSort.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class T0000_QuickSort {
+
+    public static void main(String[] args) {
+        int[] arr = {1,9,4,7,3,0};
+        System.out.println(Arrays.toString(sort(arr)));
+    }
+
+    public static int[] sort(int[] sourceArray) {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        return quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static int[] quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(arr, left, right);
+            quickSort(arr, left, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, right);
+        }
+        return arr;
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+        // 设定基准值（pivot）
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (arr[i] < arr[pivot]) {
+                swap(arr, i, index);
+                index++;
+            }
+        }
+        swap(arr, pivot, index - 1);
+        return index - 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
