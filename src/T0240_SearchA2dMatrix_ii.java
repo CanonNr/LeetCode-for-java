cef5fad566b613c7c04d159db5c13177793f1848
canon <717437709@qq.com>
2021-10-26 13:53:22 +0800

    240. 搜索二维矩阵ii

--- src/T0240_SearchA2dMatrix_ii.java
+++ src/T0240_SearchA2dMatrix_ii.java
@@ -1,29 +1,36 @@
 public class T0240_SearchA2dMatrix_ii {
     public static void main(String[] args) {
-//        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
-        int[][] matrix = {{1,1}};
-        int target = 2;
+        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+//        int[][] matrix = {{1},{6}};
+//        int[][] matrix = {{-1,3}};
+//        int[][] matrix = {{-5}};
+        int target = 5;
         boolean r = searchMatrix(matrix,target);
         System.out.println(r);
     }
 
-    public static boolean searchMatrix(int[][] matrix, int target) {
-        int l = 0;
-        for (int i = 0 ; i < matrix[0].length ; i++ ) {
-            // 相等直接返回true
-            if (matrix[l][i] == target) return true;
-            // 遇到大于则从上一位开始往下找
-            if (matrix[l][i] > target){
-                if (l<=0) return false;
-                for (int j = 0; j < matrix.length; j++) {
-                    if (matrix[j][l] == target) return true;
-                    if (matrix[j][l] > target) return false;
-                }
+    /**
+     * 执行用时：15 ms, 在所有 Java 提交中击败了8.89%的用户
+     * 内存消耗：43.8 MB, 在所有 Java 提交中击败了66.83%的用户
+     * 通过测试用例：129 / 129
+     * 从左上角第一个元素开始，从上往下比较，如果到底或者大于target值则进入下一列。
+     */
+    public static boolean searchMatrix(int[][] matrix, int target){
+        for (int i = 0; i < matrix[0].length; i++) {
+            int x = 0;
+            if(matrix[x][i] > target){
+                return false;
             }else {
-                l++;
+                for (x = 0; x < matrix.length; x++) {
+                    if (matrix[x][i] == target){
+                        return true;
+                    }else if(matrix[x][i] > target){
+                        break;
+                    }
+                }
             }
         }
-
         return false;
     }
+
 }
